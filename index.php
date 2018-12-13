4e88a983c0a64df4d304996c887782206c3cecc3
unknown <perdiwono@gmail.com>
2018-12-13 13:09:26 +0700

    project pertama

---
+++ index.php
@@ -0,0 +1,174 @@
+<!doctype html>
+<html lang="en">
+  <head>
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+
+    <!-- Bootstrap CSS -->
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css" integrity="sha384-MCw98/SFnGE8fJT3GXwEOngsV7Zt27NXFoaoApmYm81iuXoPkFOJwJ8ERdknLPMO" crossorigin="anonymous">
+
+    <!-- my css -->
+    <link rel="stylesheet" type="text/css" href="style.css">
+
+    <link href="https://fonts.googleapis.com/css?family=Viga" rel="stylesheet">
+
+    <title>Task1</title>
+  </head>
+  <body>
+      <!-- NAVBAR -->
+        <nav class="navbar navbar-expand-lg navbar-light">
+          <div class="container">
+          <a class="navbar-brand" href="#">BecomeStudent</a>
+  <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
+    <span class="navbar-toggler-icon"></span>
+  </button>
+  <div class="collapse navbar-collapse" id="navbarNavAltMarkup">
+    <div class="navbar-nav ml-auto">
+      <a class="nav-item nav-link active" href="#">Home <span class="sr-only">(current)</span></a>
+      <a class="nav-item nav-link" href="#">About</a>
+      <a class="nav-item nav-link" href="#">Setting</a>
+      <a class="nav-item nav-link" href="#">Profil</a>
+      <a class="nav-item btn btn-danger tombol" href="#">Sign In</a>
+    </div>
+  </div>
+</div>
+</nav>
+      <!-- AKHIR NAVBAR -->
+<div class="jumbotron jumbotron-fluid">
+</div>
+   <div class="baris">
+    <img src="img/ape.png">
+    <p class="trailer">Trailer Terbaru</p>
+      <div class="embed-responsive embed-responsive-1by1">
+      <iframe width="560" height="315" src="https://www.youtube.com/embed/wuIbeQv3v7c" frameborder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+      </div>
+      <div class="kolom">
+        <h3>Dragon Ball Super Broly Movie 2018 New Trailer! 4</h3>
+        <p>“Ini adalah kisah dari Saiyan baru.
+Bumi sedang damai sejak berakhirnya Tournament of Power. Menyadari setiap semesta masih memiliki petarung kuat yang belum ia lihat, Goku menghabiskan waktunya setiap hari untuk berlatih untuk mencapai level kekuatan yang lebih tinggi lagi.
+Suatu hari, Goku dan Vegeta berhadapan dengan Saiyan bernama ‘Broly’ yang belum pernah mereka lihat sebelumnya.
+Kaum Saiyan seharusnya sudah hampir musnah dalam kehancuran planet Vegeta, jadi apa yang sosok ini lakukan di Bumi?
+Pertemuan antara tiga Saiyan dengan takdir berbeda ini berkembang menjadi pertempuran seru dengan Frieza (baru saja kembali dari neraka) turut terlibat.”
+Sinopsis Dragon Ball Super: Broly ini menegaskan sekali lagi kalau ini adalah pertemuan resmi pertama Goku dan Vegeta melawan Broly.
+Jadi Broly yang dulu dikalahkan Goku dengan kumpulan kekuatan teman-temannya, lalu diterbangkan Kamehameha Gohan, Goten, dan hantu Goku ke matahari itu beda dunia dari yang ini.
+Sinopsis ini juga menyorot kalau seharusnya kaum Saiyan sudah banyak yang musnah setelah kehancuran Planet Vegeta.
+Yang tersisa awalnya hanya Goku, Vegeta, Nappa, dan Raditz. Sekarang yang sisa ya Goku, Vegeta, saudaranya Vegeta, dan keluarga mereka.
+Kalau di movie tahun 1993 sih, Broly dan Paragus – ayahnya – dieksekusi karena dianggap bisa membahayakan kekuasaan keluarga kerajaan.
+Namun saat Planet Vegeta hancur, kekuatan Broly kecil bangkit dan dia bisa melindungi dirinya dan ayahnya.</p>
+        <a href="" class="btn btn-primary tombol1">full trailer movie</a>
+      </div>
+    </div>
+    <section class="container1 pt-3">
+    <div class="row">
+        <div class="col-lg-12">
+            <img src="img/rekomendasi.png">
+        </div>
+    </div>
+</section>
+<section class="carousel slide" data-ride="carousel" id="postsCarousel">
+    <div class="container">
+        <div class="row">
+            <div class="col-12 text-md-right lead">
+                <a class="btn btn-secondary-outline prev" href="" title="go back"><i class="fa fa-lg fa-chevron-left"></i></a>
+                <a class="btn btn-secondary-outline next" href="" title="more"><i class="fa fa-lg fa-chevron-right"></i></a>
+            </div>
+        </div>
+    </div>
+    <div class="container pt-0 mt-2">
+        <div class="carousel-inner">
+            <div class="carousel-item active">
+                <div class="card-deck">
+                <div class="card h-100">
+                    <div class="card-img-top card-img-top-250">
+                        <img class="img-fluid" src="img/nanat.jpg" alt="Carousel 1">
+                    </div>
+                    <div class="card-body pt-2">
+                        <h6 class="small text-wide p-b-2">Fantasi</h6>
+                        <h2>
+                            <a href="">Nanatsu No Taizai</a>
+                        </h2>
+                    </div>
+                </div>
+                <div class="card h-100">
+                    <div class="card-img-top card-img-top-250">
+                        <img class="img-fluid" src="img/narut.jpg" alt="Carousel 2">
+                    </div>
+                    <div class="card-body pt-2">
+                        <h6 class="small text-wide p-b-2">Shounen</h6>
+                        <h2>
+                            <a href="">Naruto The Movie</a>
+                        </h2>
+                    </div>
+                </div>
+                <div class="card h-100">
+                    <div class="card-img-top card-img-top-250">
+                        <img class="img-fluid" src="img/onepiee.jpg" alt="Carousel 3">
+                    </div>
+                    <div class="card-body pt-2">
+                        <h6 class="small text-wide p-b-2">Adventure</h6>
+                        <h2>
+                            <a href="">One Piece Marine</a>
+                        </h2>
+                    </div>
+                </div>
+            </div>
+            </div>
+            <div class="carousel-item">
+                <div class="card-deck">
+                <div class="card">
+                    <div class="card-img-top card-img-top-250">
+                        <img class="img-fluid" src="img/ace.jpg" alt="Carousel 4">
+                    </div>
+                    <div class="card-body pt-2">
+                        <h6 class="small text-wide p-b-2">Seinen</h6>
+                        <h2>
+                            <a href="">Ansatsu Kyoushitsu</a>
+                        </h2>
+                    </div>
+                </div>
+                <div class="card">
+                    <div class="card-img-top card-img-top-250">
+                        <img class="img-fluid" src="img/sai.jpg" alt="Carousel 5">
+                    </div>
+                    <div class="card-body pt-2">
+                        <h6 class="small text-wide p-b-2"><span class="pull-xs-right">SuperHero</h6>
+                        <h2>
+                            <a href="">One Punch Man</a>
+                        </h2>
+                    </div>
+                </div>
+                <div class="card">
+                    <div class="card-img-top card-img-top-250">
+                        <img class="img-fluid" src="img/bo.jpg" alt="Carousel 6">
+                    </div>
+                    <div class="card-body pt-2">
+                        <h6 class="small text-wide p-b-2">SuperNatural</h6>
+                        <h2>
+                            <a href="">Boku No Hero</a>
+                        </h2>
+                    </div>
+                </div>
+            </div>
+            </div>
+        </div>
+    </div>
+</section>
+<footer>
+    <p class="footer">Hak Cipta 2018 © Become Student</p>
+  </footer>
+    <!-- Optional JavaScript -->
+    <!-- jQuery first, then Popper.js, then Bootstrap JS -->
+    <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.3/umd/popper.min.js" integrity="sha384-ZMP7rVo3mIykV+2+9J3UJ46jBk0WLaUAdn689aCwoqbBJiSnjAK/l8WvCWPIPm49" crossorigin="anonymous"></script>
+    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/js/bootstrap.min.js" integrity="sha384-ChfqqxuZUCnJSK3+MXmPNIyE6ZbWh2IMqE241rYiqJxyMiZ6OW/JmZQ5stwEULTy" crossorigin="anonymous"></script>
+    <script>
+      (function($) {
+    "use strict";
+    // manual carousel controls
+    $('.next').click(function(){ $('.carousel').carousel('next');return false; });
+    $('.prev').click(function(){ $('.carousel').carousel('prev');return false; });
+})(jQuery);
+    </script>
+  </body>
+</html>
